bffac75a73aadf728ee5ed86f86620639fa51751
NDacevic <n.dacevic@gmail.com>
2020-05-14 11:16:24 +0200

    added initial functionality for connecting to the remote database

--- ApiTestApp_Grupp3/ApiTestApp_Grupp3/Startup.cs
+++ ApiTestApp_Grupp3/ApiTestApp_Grupp3/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +26,9 @@ namespace ApiTestApp_Grupp3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContextPool< ****** NEEDS DB CONTEXT ****** >(
+            //options => options.UseSqlServer(Configuration.GetConnectionString("MainDBConnection"));
+
             services.AddControllers();
         }
 
