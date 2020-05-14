45351d7824f82b5c612e364e1a0262d946d420a7
NDacevic <n.dacevic@gmail.com>
2020-05-14 11:43:13 +0200

    Added MVC to services

--- ApiTestApp_Grupp3/ApiTestApp_Grupp3/Startup.cs
+++ ApiTestApp_Grupp3/ApiTestApp_Grupp3/Startup.cs
@@ -30,6 +30,7 @@ namespace ApiTestApp_Grupp3
             //options => options.UseSqlServer(Configuration.GetConnectionString("MainDBConnection"));
 
             services.AddControllers();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
