e939d10a9fc1e8ad01ae209299e0aa1243873c46
JohnnyK <56879977+JohnnyKrook@users.noreply.github.com>
2020-05-19 13:50:00 +0200

    Updated GetTest

--- ApiTestApp_Grupp3/ApiTestApp_Grupp3/Models/Question.cs
+++ ApiTestApp_Grupp3/ApiTestApp_Grupp3/Models/Question.cs
@@ -37,5 +37,6 @@ namespace ApiTestApp_Grupp3.Models
         //instead of having an extra class on the client side and downloading the Courses as objects
         [NotMapped]
         public string CourseName { get; set; }
+
     }
 }
