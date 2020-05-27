679076986fba1d75fad8c4206f7673d9cd99dbd7
NDacevic <n.dacevic@gmail.com>
2020-05-27 09:21:17 +0200

    Migrationfiles have changed. Textformatting in the DbContext.

--- ApiTestApp_Grupp3/ApiTestApp_Grupp3/Data/ApiTestApp_Grupp3Context.cs
+++ ApiTestApp_Grupp3/ApiTestApp_Grupp3/Data/ApiTestApp_Grupp3Context.cs
@@ -68,6 +68,7 @@ namespace ApiTestApp_Grupp3.Data
         public DbSet<ApiTestApp_Grupp3.Models.TestQuestion> TestQuestion { get; set; }
 
         public DbSet<ApiTestApp_Grupp3.Models.Role> Role { get; set; }
+
         public DbSet<ApiTestApp_Grupp3.Models.EmployeeRole> EmployeeRole { get; set; }
     }
 }
